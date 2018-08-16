ea9b0c62b68345d684942b43fa50209ab848c7bf
MohsenAfana <you@example.com>
2018-08-16 12:08:45 +0300

    firebase register

---
+++ app/src/main/java/hammoud/sami/mhmd/moviesmaster/interets_activity.java
@@ -0,0 +1,13 @@
+package hammoud.sami.mhmd.moviesmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class interets_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interets_activity);
+    }
+}
